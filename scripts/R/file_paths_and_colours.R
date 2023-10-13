66d7e0ac1fc64b38745dc2c9a97e719d0d7f79ba
John Fryer, Ph.D <106278420+fryerlab@users.noreply.github.com>
2023-10-13 11:44:16 -0700

    Update file_paths_and_colours.R

--- scripts/R/file_paths_and_colours.R
+++ scripts/R/file_paths_and_colours.R
@@ -1,5 +1,12 @@
-# load libraries
+#----------------------
+# Load libraries
+#     Libraries may by running `install.packages("packageName")` or  BiocManager::install("packageName")
+#     Some libraries were installed from git repos:
+#        install.packages("remotes")
+#        remotes::install_github("GabrielHoffman/mvIC")
+#-----------------------
 library(ComplexUpset)
+library("llapack")
 library(gprofiler2)
 library(airway)
 library(enrichplot)
@@ -16,23 +23,23 @@ library(ggrepel)
 library(ggplot2)
 library(gplots)
 library(grDevices)
-require(philentropy) #install.packages("philentropy")
+require(philentropy) 
 library(rtracklayer)
 library(stringr)
-require(variancePartition) # BiocManager::install("variancePartition")
+require(variancePartition) 
 library(reshape)
 library(Glimma)
 library(plyr)
 library(corrplot)
 library(ggpubr)
-library(tidyverse, lib.loc = "/usr/local/biotools/rpackages/R-4.2.2-2023-02-01")
+library(tidyverse)
 library(caret)
 library(glmnet)
 library(vroom)
 library(matrixStats)
 library("data.table")
 library(DESeq2)
-library(dittoSeq) #BiocManager::install("dittoSeq")
+library(dittoSeq) 
 library(Hmisc)
 library(tidyr)
 library(gridExtra)
@@ -41,30 +48,11 @@ require(openxlsx)
 library(UpSetR)
 library(mvIC) 
 library(RColorBrewer)
-library(tidyr, lib.loc = "/usr/local/biotools/rpackages/R-4.1.2-2021-11-11")
-library(vctrs, lib.loc = "/usr/local/biotools/rpackages/R-4.2.2-2023-02-01")
-# Install information
-#library(vctrs, lib.loc = "/usr/local/biotools/rpackages/R-4.2.2-2023-02-01")
-#install.packages("remotes")
-#remotes::install_github("GabrielHoffman/mvIC")
-#library("llapack")
-#remotes::install_github("GabrielHoffman/mvIC")
-#BiocManager::install("ShrinkCovMat")
-#devtools::install_github("gzt/CholWishart", lib = "/usr/local/biotools/rpackages/R-4.2.2-2023-02-01")
-#install.packages('CholWishart')
-#library(wesanderson)
-#library(DEGreport)
+library(vctrs)
 
-# To install run:
-#devtools::install_github('dviraran/xCell')
-# devtools::install_github("hagenaue/BrainInABlender")
-library(xCell)
-library(devtools)
-#library(BrainInABlender) # see https://github.com/hagenaue/BrainInABlender 
-source(here::here("/research/labs/neurology/fryer/m239830/tools/BrainInABlender/R/Sir_UnMixALot.R"))
-library(reshape2)
-
-# paths, colors, shapes and more
+#-----------------------
+# paths, colors, shapes
+#-----------------------
 LBD <- "LBD"
 AD <- "AD"
 PA <- "PA"
@@ -78,65 +66,22 @@ AD_shape <- c(16) # circle
 PA_shape <- c(17) # triangle
 LBD_shape <- c(18) # diamond
 
-TypeColors <- c("#4682B4", "#B4AF46","#B4464B", "gray35")
-SexColors <- c("#490092", "#D55E00")
+TypeColors <- c("#4682B4", "#B4AF46","#B4464B", "gray35") # disease type colors 
+SexColors <- c("#490092", "#D55E00") # orange and purple
 colorbindColors <- dittoColors()
-correlationColors <-
-  colorRampPalette(c("#4477AA", "#77AADD", "#FFFFFF", "#EE9988", "#BB4444"))
+correlationColors <- colorRampPalette(c("#4477AA", "#77AADD", "#FFFFFF", "#EE9988", "#BB4444"))
 
+pathToRef = c("/LBD_CWOW/references/")
+pathToRawData = c("/LBD_CWOW/raw_fastq/")
 
-pathToRef = c("/research/labs/neurology/fryer/projects/references/human/")
-pathToRawData = c("/research/labs/neurology/fryer/projects/LBD_CWOW/")
-
+# Save files as PDFs
 saveToPDF <- function(...) {
   d = dev.copy(pdf, ...)
   dev.off(d)
 }
 
 # read in metadata
-# the expanded metadata contains inferred sex, RIN, and WGS sample IDs
-# metadata <- read.delim(paste0(pathToRawData, "RNA_metadata.tsv"))
-# read in metadata with A-T-S score information
-metadata <-
-  read.delim(
-    "/research/labs/neurology/fryer/m239830/LBD_CWOW/rObjects/metadata_A-T-S_scores.txt"
-  )
-# keep NPID and ATS information to merge with BinB metadata
-#keep <- c("NPID", "A", "T", "S", "ATS", "ATS_names")
-#df <- metadata_ATS[, (names(metadata_ATS) %in% keep)]
+metadata <- read.delim("scripts/RNA_metadata.tsv"))
 
-# Note that BinB cell type information is made in cell_type_enrichment script
-#metadata <-
-#  read.delim(
- #   "/research/labs/neurology/fryer/m239830/LBD_CWOW/rObjects/metadata_BinB_cellType_Zscore.txt"
- # )
-
-# umbrella cell types rather than primary cell types 
-# metadata_BinB_umbrella_cellType_Zscore.txt
-metadata <-
-  read.delim(
-    "/research/labs/neurology/fryer/m239830/LBD_CWOW/rObjects/metadata_BinB_umbrella_cellType_Zscore.txt"
-  )
-metadata$lib.size.2 <- NULL
-metadata$norm.factors.2 <- NULL
-metadata$lib.size.1 <- NULL
-metadata$norm.factors.1 <- NULL
-#metadata$lib.size <- NULL
-#metadata$norm.factors <- NULL
 # set factor levels
-metadata$TYPE <-
-  factor(metadata$TYPE, levels = c("CONTROL", "PA", "AD", "LBD"))
-metadata$ATS_names <-
-  factor(
-    metadata$ATS_names,
-    levels = c(
-      "no pathology",
-      "amyloid",
-      "amyloid + tau",
-      "pure synuclein",
-      "low amyloid + synuclein",
-      "high amyloid + synuclein",
-      "amyloid + synuclein + tau"
-    )
-  )
-
+metadata$TYPE <- factor(metadata$TYPE, levels = c("CONTROL", "PA", "AD", "LBD"))
